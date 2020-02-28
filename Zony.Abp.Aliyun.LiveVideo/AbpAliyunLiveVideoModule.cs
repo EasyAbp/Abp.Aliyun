1c2862a3266b10e4d5af7579411138b0efd60f2c
GameBelial <243387971@qq.com>
2020-02-28 14:19:03 +0800

    feat(视频直播): 建立基本的项目结构。

---
+++ Zony.Abp.Aliyun.LiveVideo/AbpAliyunLiveVideoModule.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.Modularity;
+using Zony.Abp.Aliyun.Common;
+
+namespace Zony.Abp.Aliyun.LiveVideo
+{
+    [DependsOn(typeof(AbpAliyunCommonModule))]
+    public class AbpAliyunLiveVideoModule : AbpModule
+    {
+    }
+}
---
+++ Zony.Abp.Aliyun.LiveVideo/DomainManage/DomainManageService.cs
@@ -0,0 +1,9 @@
+using Zony.Abp.Aliyun.Common.Services;
+
+namespace Zony.Abp.Aliyun.LiveVideo.Services
+{
+    public class AliyunLiveVideoDomainService : BaseAliyunService
+    {
+        
+    }
+}
