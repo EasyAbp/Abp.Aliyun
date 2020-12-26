137088ae07a8910a589b9cfedf8f5c8174294105
gdlcf88 <gdlcf88@outlook.com>
2020-12-26 18:10:46 +0800

    Upgrade to ABP 4.0.2

--- src/EasyAbp.Abp.Aliyun.Common/AbpAliyunCommonModule.cs
+++ src/EasyAbp.Abp.Aliyun.Common/AbpAliyunCommonModule.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.Guids;
+using Volo.Abp.Json;
 using Volo.Abp.Modularity;
 
 namespace EasyAbp.Abp.Aliyun.Common
 {
-    [DependsOn(typeof(AbpGuidsModule))]
+    [DependsOn(
+        typeof(AbpGuidsModule),
+        typeof(AbpJsonModule))]
     public class AbpAliyunCommonModule : AbpModule
     {
         public override void ConfigureServices(ServiceConfigurationContext context)
